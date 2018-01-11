f523ed6f712b05df26d075aadda6e5f71e4ca6c2
Josh Coles <josh@joshcoles.com>
2018-01-11 13:03:55 -0800

    Finished styling for tablet.

--- assets/scripts/about-section-display.js
+++ assets/scripts/about-section-display.js
@@ -1,7 +1,18 @@
 $(function () {
+
+
   $('#about-select').change(function () {
     console.log("changed");
     $('.about-section').hide();
     $('#' + $(this).val()).show();
   });
+
+  $('#about-tabs-container .about-tab').on('click', function(){
+    var stripped = $(this).attr('id').substring(0, $(this).attr('id').length - 4);
+    $('.about-tab').removeClass('active');
+    $(this).addClass('active');
+    $('.about-section').hide();
+    $('#' + stripped).show();
+  })
+
 });
