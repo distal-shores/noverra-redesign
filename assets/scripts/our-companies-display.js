11b4c2679ef3c4a33f45d957e8ffddcee72d10f9
Josh Coles <josh@joshcoles.com>
2018-01-12 12:20:21 -0800

    Finished 'our companies' for all screen sizes.

--- assets/scripts/our-companies-display.js
+++ assets/scripts/our-companies-display.js
@@ -3,4 +3,12 @@ $(function () {
     $('.companies-section').hide();
     $('#' + $(this).val()).show();
   });
+
+  $('#companies-tabs-container .tab').on('click', function () {
+    var stripped = $(this).attr('id').substring(0, $(this).attr('id').length - 4);
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('.companies-section').hide();
+    $('#' + stripped).show();
+  })
 });
