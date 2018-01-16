697b0ab9db64bab5a50a24b94d60db4892c8680c
Warren Wegner <warrenwegner@gmail.com>
2018-01-16 00:27:45 -0800

    adds support for subsection breadcrumbs, refactored js to allow linking to subsections, this created a scroll issue that someone will have to work out

--- _plugins/drops/breadcrumb_item.rb
+++ _plugins/drops/breadcrumb_item.rb
@@ -5,18 +5,27 @@ module Drops
     def_delegator :@page, :data
     def_delegator :@page, :url
 
-    def initialize(page, payload)
+    def initialize(page, payload, hash = "", titleOverride = nil)
       @payload = payload
       @page = page
+      @hash = hash
+      @titleOverride = titleOverride
     end
 
     def title
-      @page.data["breadcrumb"] != nil ? @page.data["breadcrumb"] : @page.data["title"]
+      if @titleOverride.nil?
+        then @page.data["breadcrumb"] != nil ? @page.data["breadcrumb"] : @page.data["title"]
+      else
+        @titleOverride
+      end
     end
 
     def namespace
       @page.data["namespace"]
     end
 
+    def getUrlHash
+      @hash
+    end
   end
 end
--- assets/scripts/our-companies-display.js
+++ assets/scripts/our-companies-display.js
@@ -1,14 +1,53 @@
 $(function () {
   $('#companies-select').change(function () {
-    $('.companies-section').hide();
-    $('#' + $(this).val()).show();
+    updateMobileViewCompanies($(this).val());
+    updateDesktopViewCompanies($(this).val());
+    updateBreadcrumbs($(this).val());
   });
 
   $('#companies-tabs-container .tab').on('click', function () {
     var stripped = $(this).attr('id').substring(0, $(this).attr('id').length - 4);
-    $('.tab').removeClass('active');
-    $(this).addClass('active');
+    updateDesktopViewCompanies(stripped);
+    updateMobileViewCompanies(stripped);
+    updateBreadcrumbs(stripped);
+  });
+
+  $(document).ready(function() {
+    $("[data-hash]").hide();
+    var hash = window.location.hash;
+    hash = hash.substr(1);
+
+    if (hash.length === 0) {
+      hash = $("[data-hash]").data('hash');
+    }
+    updateDesktopViewCompanies(hash);
+    updateMobileViewCompanies(hash);
+    updateBreadcrumbs(hash);
+  });
+
+  var updateMobileViewCompanies = function(hash) {
     $('.companies-section').hide();
-    $('#' + stripped).show();
-  })
+    $('#' + hash).show();
+  };
+
+  var updateDesktopViewCompanies = function(hash) {
+    $('.tab').removeClass('active');
+    $("#" + hash + "-tab").addClass('active');
+    $('.companies-section').hide();
+    $('#' + hash).show();
+  };
+
+  var updateBreadcrumbs = function(hash) {
+    $("[data-hash]").hide();
+    $("[data-hash='" + hash + "']").show();
+    window.location.hash = '#' + hash;
+    // Prevents scrolling to the anchor on hash change
+    if (location.hash) {
+      window.scrollTo(0, 0);
+      setTimeout(function() {
+
+        window.scrollTo(0, 0);
+      }, 1);
+    }
+  }
 });
