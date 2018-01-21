9e3f14df09934578ba63dafe294e324d421cabcf
Sam Coll <samcoll@gmail.com>
2018-01-21 14:59:14 -0800

    Implement a folding nav on page scroll.

--- assets/scripts/scrollTop.js
+++ assets/scripts/scrollTop.js
@@ -1,10 +1,26 @@
 $(function () {
   var $scrollTop = $(".scrollTop");
 
+  function foldNav() {
+    $('.main-nav').css('height','45px');
+    $('.breadcrumb').css('height','35px');
+    $('.arrow-right').css({'border-top-width':'17.5px','border-bottom-width':'17.5px'});
+    $('.dropdown i').css({'right':'25px', 'left':'initial','bottom':'initial', 'top':'50%', 'transform':'translateY(-50%)', 'width':'5px', 'display':'inline'});
+  }
+
+  function unfoldNav() {
+    $('.main-nav').css('height','90px');
+    $('.breadcrumb').css('height','50px');
+    $('.arrow-right').css({'border-top-width':'25px','border-bottom-width':'25px'});
+    $('.dropdown i').css({'right':'initial', 'top':'initial', 'left':'0', 'bottom':'0', 'width':'100%', 'transform':'none', 'display':'inline-block'});
+  }
+
   $( window ).scroll(function() {
     if( $(document).scrollTop() > 300 ) {
         $scrollTop.show();
+        foldNav();
     } else {
+        unfoldNav();
         $scrollTop.hide();
     }
   });
